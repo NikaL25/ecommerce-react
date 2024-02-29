29c77e4d606507e944a4b8a3aa28b509c1b89e23
nika-lomiashvili <mr.lomiashvili@mail.ru>
2024-02-29 08:09:17 +0400

    ecommerce-react

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import AppRoutes from "./components/Routes/Routes"
+import Header from "./components//Header/Header";
+import Footer from "./components/Footer/Footer";  
+import Sidebar from "./components/Sidebar/Sidebar";
+
+import { getCategories } from "./features/categories/categoriesSlice";
+import { getProducts } from "./features/products/productsSlice";
+
+import UserForm from "./components/User/UserForm";
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCategories());
+    dispatch(getProducts());
+  }, [dispatch]);
+
+  return (
+    <div className="app">
+      <Header />
+      <UserForm />
+      <div className="container">
+        <Sidebar />
+        <AppRoutes />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+import Modal from "react-modal";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addItemToCart,
+  removeItemFromCart,
+} from "../../features/user/userSlice";
+
+import styles from "../../styles/Cart.module.css";
+import { sumBy } from "../../utils/common";
+import { Link } from "react-router-dom";
+
+const Cart = () => {
+  const dispatch = useDispatch();
+  const { cart } = useSelector(({ user }) => user);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const changeQuantity = (item, quantity) => {
+    dispatch(addItemToCart({ ...item, quantity }));
+  };
+
+  const removeItem = (id) => {
+    dispatch(removeItemFromCart(id));
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  return (
+    <section className={styles.cart}>
+      <h2 className={styles.title}>Your cart</h2>
+
+      {!cart.length ? (
+        <div className={styles.empty}>Here is empty</div>
+      ) : (
+        <>
+          <div className={styles.list}>
+            {cart.map((item) => {
+              const { title, category, images, price, id, quantity } = item;
+
+              return (
+                <div className={styles.item} key={id}>
+                  <div
+                    className={styles.image}
+                    style={{ backgroundImage: `url(${images[0]})` }}
+                  />
+                  <div className={styles.info}>
+                    <h3 className={styles.name}>{title}</h3>
+                    <div className={styles.category}>{category.name}</div>
+                  </div>
+
+                  <div className={styles.price}>{price}$</div>
+
+                  <div className={styles.quantity}>
+                    <div
+                      className={styles.minus}
+                      onClick={() =>
+                        changeQuantity(item, Math.max(1, quantity - 1))
+                      }
+                    >
+                      <svg className="icon">
+                        <use
+                          xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#minus`}
+                        />
+                      </svg>
+                    </div>
+
+                    <span>{quantity}</span>
+
+                    <div
+                      className={styles.plus}
+                      onClick={() =>
+                        changeQuantity(item, Math.max(1, quantity + 1))
+                      }
+                    >
+                      <svg className="icon">
+                        <use
+                          xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#plus`}
+                        />
+                      </svg>
+                    </div>
+                  </div>
+
+                  <div className={styles.total}>{price * quantity}$</div>
+
+                  <div
+                    className={styles.close}
+                    onClick={() => removeItem(item.id)}
+                  >
+                    <svg className="icon">
+                      <use
+                        xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#close`}
+                      />
+                    </svg>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+
+          <div className={styles.actions}>
+            <div className={styles.total}>
+              TOTAL PRICE:{" "}
+              <span>
+                {sumBy(cart.map(({ quantity, price }) => quantity * price))}$
+              </span>
+            </div>
+
+            <button className={styles.proceed} onClick={openModal}>
+              Proceed to checkout
+            </button>
+          </div>
+        </>
+      )}
+
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        className={styles.modal}
+        overlayClassName={styles.overlay}
+      >
+        <div style={{background: '' , display: "flex", justifyContent: "center", alignItems: "center", position: "fixed",  top: 0,  left: 0, right: 0,  bottom: 0}}>
+         
+          <Link to='/'>
+           <button onClick={closeModal}>Successfull Payment</button>
+          </Link>
+         
+        </div>
+      </Modal>
+    </section>
+  );
+};
+
+export default Cart;
